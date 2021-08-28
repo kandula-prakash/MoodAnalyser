a2cb8e11d28627475583ec7e4802b6a8e3167e90
kandula-prakash <abhinayprakash143@gmail.com>
2021-08-28 17:15:07 +0530

    feat:moodAnalyser

---
+++ MoodAnalyserFactory.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace MoodAnalyzer
+{
+   public class MoodAnalyserFactory
+    {
+        public static object CreateMoodAnalyse(string className, string constructorName)
+        {
+            string pattren = @"." + constructorName + "$";
+            Match result = Regex.Match(className, pattren);
+            if (result.Success)
+            {
+                try
+                {
+                    Assembly executing = Assembly.GetExecutingAssembly();
+                    Type moodAnalyseType = executing.GetType(className);
+                    return Activator.CreateInstance(moodAnalyseType);
+
+                }
+                catch (ArgumentNullException)
+                {
+
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_CLASS, "class not found");
+                }
+
+            }
+            else
+            {
+                throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+            }
+           
+        }
+          
+            public static object CreateMoodAnalyseUsingParameterizedConstructor(string className, string constructorName, string message)
+            {
+                Type type = typeof(moodAnalyze);
+                if (type.Name.Equals(className) || type.FullName.Equals(className))
+                {
+                    if (type.Name.Equals(constructorName))
+                    {
+                        ConstructorInfo ctor = type.GetConstructor(new[] { typeof(string) });
+                        object instance = ctor.Invoke(new object[] { message });
+                        return instance;
+                    }
+                    else
+                    {
+                        throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_METHOD, "Constructor is not found");
+                    }
+                }
+                else
+                {
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NO_SUCH_CLASS, "Class not found");
+
+                }
+            }
+
+        public static object CreateMoodAnalyser(string v1, string v2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    }
+
+   
+
---
+++ moodAnalyze.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyzer
+{
+   public class moodAnalyze
+    {
+        private string message;
+            public moodAnalyze(string message)
+        {
+            this.message = message; 
+        }
+        public string AnalyzeMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.EMPTY_MESSAGE, "Should Not BeEmpty");
+
+                }
+
+           
+            if (this.message.Contains ("sad"))
+            {
+                return "sad";
+
+            }
+            else
+            {
+                return "Happy";
+            }
+            }
+            catch (NullReferenceException)
+            {
+
+                throw new MoodAnalyzerCustomException(MoodAnalyzerCustomException.ExceptionType.NULL_MESSAGE, "mood should not be null");
+            }
+        }
+    }
+}
